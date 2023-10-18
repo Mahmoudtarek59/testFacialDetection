ca376e99444dbfec6afd125d4a363212ddaf17b4
MahmoudTarek59 <tmahmoud974@gmail.com>
2023-10-18 15:40:47 +0300

    First Commit

---
+++ android/app/src/main/kotlin/com/example/testfacede/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testfacede
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/vision_detector_views/face_detector_view.dart
@@ -0,0 +1,96 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+
+import 'detector_view.dart';
+import 'painters/face_detector_painter.dart';
+
+class FaceDetectorView extends StatefulWidget {
+  @override
+  State<FaceDetectorView> createState() => _FaceDetectorViewState();
+}
+
+class _FaceDetectorViewState extends State<FaceDetectorView> {
+  final FaceDetector _faceDetector = FaceDetector(
+    options: FaceDetectorOptions(
+      enableContours: false,
+      enableLandmarks: true,
+      performanceMode: FaceDetectorMode.accurate,
+      enableClassification: true,
+
+      // enableTracking: true
+
+    ),
+  );
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? _customPaint;
+  String? _text;
+  var _cameraLensDirection = CameraLensDirection.front;
+
+  @override
+  void dispose() {
+    _canProcess = false;
+    _faceDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DetectorView(
+      title: 'Face Detector',
+      customPaint: _customPaint,
+      text: _text,
+      onImage: _processImage,
+      initialCameraLensDirection: _cameraLensDirection,
+      onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+    );
+  }
+
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    List<Face> faces = await _faceDetector.processImage(inputImage);
+    print("testtttt ${faces.length}");
+    //todo only one face...
+    // if(faces.length==1){
+      if (inputImage.metadata?.size != null &&
+          inputImage.metadata?.rotation != null) {
+        final painter = FaceDetectorPainter(
+          faces,
+          inputImage.metadata!.size,
+          inputImage.metadata!.rotation,
+          _cameraLensDirection,
+        );
+        _customPaint = CustomPaint(painter: painter);
+      } else {
+        String text = 'Faces found: ${faces.length}\n\n';
+        for (final face in faces) {
+          text += 'face: ${face.boundingBox}\n\n';
+        }
+        _text = text;
+        // TODO: set _customPaint to draw boundingRect on top of image
+        _customPaint = null;
+      }
+      _isBusy = false;
+      if (mounted) {
+        setState(() {});
+      }
+    // }else if (faces.length>1){
+
+      //todo can't capture image ....
+      //todo bool variable to show error flag
+
+    // }
+    // setState(() {
+    //
+    // });
+    print("testtttt ${_text}");
+    ///////////////////////////////
+
+  }
+}
